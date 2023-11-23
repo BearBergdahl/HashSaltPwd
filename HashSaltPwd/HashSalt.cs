a92164c353bea6d7d90515a447d492985579fd2c
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-23 12:52:56 +0100

    Add project files.

---
+++ HashSaltPwd/HashSalt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSaltPwd
+{
+    public  class HashSalt
+    {
+        const int keySize = 64;
+        const int iterations = 350000;
+        public byte[] salt = null;
+        HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+        public string HashPassword(string password)
+        {
+            salt = RandomNumberGenerator.GetBytes(keySize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(password),
+                salt,
+                iterations,
+                hashAlgorithm,
+                keySize);
+            return Convert.ToHexString(hash);
+        }
+        //Save salt somewhere!
+
+        public bool Verify(string password, string hash)
+        {
+            return VerifyPassword(password, hash, salt);
+        }
+
+        private bool VerifyPassword(string password, string hash, byte[] salt)
+        {
+            var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, hashAlgorithm, keySize);
+            return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromHexString(hash));
+        }
+
+    }
+}
---
+++ HashSaltPwd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace HashSaltPwd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("https://code-maze.com/csharp-hashing-salting-passwords-best-practices/");
+            HashSalt brownie = new HashSalt();
+            Console.WriteLine("Write your password: ");
+            string password = Console.ReadLine();
+            string hash = brownie.HashPassword(password);
+
+            bool OK = brownie.Verify(password, hash);
+
+        }
+    }
+}
